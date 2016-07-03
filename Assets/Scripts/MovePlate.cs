b022821b7fe7dbfbfeadb4d1c68e13fbfe878392
congshenV <yycmessi@163.com>
2016-07-03 16:51:00 +0800

    add the project

---
+++ Assets/Scripts/MovePlate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlate : MonoBehaviour {
+    public float speed = 5.0f;//the speed of plate
+	// Use this for initialization	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");//get the horizontal input
+        transform.Translate(Vector3.right * h * speed * Time.deltaTime);//move the plate
+	}
+}
